d65ca073e0588873d502489f7fe879b798691a53
Will Rutland <wrutland07@gmail.com>
2026-05-18 21:32:05 +0100

    updated footer

--- src/components/multiPageComponents/Footer.tsx
+++ src/components/multiPageComponents/Footer.tsx
@@ -6,6 +6,9 @@ export default function Footer():React.ReactElement {
     return (
         <React.Fragment>
             <img src={"logo.svg"} alt={"Tinbob655 website logo"} />
+            <p>
+                Website brought to you by <a href={"https://github.com/tinbob655"} style={{textDecoration: 'underLine'}}>Tinbob655</a>.
+            </p>
         </React.Fragment>
     )
 }
