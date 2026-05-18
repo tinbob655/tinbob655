f1eb12e890dbc01325f7d4edd43398f7af5ca333
Will Rutland <wrutland07@gmail.com>
2026-05-18 21:30:47 +0100

    created page header

--- src/components/multiPageComponents/PageHeader.tsx
+++ src/components/multiPageComponents/PageHeader.tsx
@@ -9,12 +9,14 @@ export default function PageHeader({title, subheading}:params):React.ReactElemen
 
     return (
         <React.Fragment>
-            <h1 className={"noVerticalSpacing"} style={{marginTop: '20px', marginLeft: '10%'}}>
+            <h1 className={"noVerticalSpacing alignLeft"} style={{marginTop: '20px', marginLeft: '10%'}}>
                 {title}
             </h1>
-            <p className={"noVerticalSpacing"} style={{marginBottom: '20px', marginLeft: '12.5%'}}>
+            <p className={"noVerticalSpacing alignLeft"} style={{marginBottom: '20px', marginLeft: '12.5%'}}>
                 {subheading}
             </p>
+
+            <div className={"dividerLine"} ></div>
         </React.Fragment>
     )
 }
