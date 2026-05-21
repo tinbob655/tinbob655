c54d5eec520082966b098b2cf0663a26c478f62c
Will Rutland <wrutland07@gmail.com>
2026-05-21 09:41:46 +0100

    footer waits for sections to appear before building

--- src/components/multiPage/footer/Footer.tsx
+++ src/components/multiPage/footer/Footer.tsx
@@ -7,27 +7,36 @@ export default function Footer():React.ReactElement {
     const [links, setLinks] = useState<React.ReactElement[]>([]);
 
     useEffect(() => {
-        const res:React.ReactElement[] = [];
+        const buildLinks = () => {
+            const headers:NodeListOf<Element> = document.querySelectorAll('.sectionHeader');
 
-        //get all sections
-        const sectionHeaders:NodeListOf<Element> = document.querySelectorAll('.sectionHeader');
-        console.log(sectionHeaders);
-        sectionHeaders.forEach((header) => {
-            res.push(
-                <div className={"footerLink"}>
+            //make sure we actually have headers
+            if (headers.length === 0) return;
+
+            //render headers' markup
+            setLinks(Array.from(headers).map(header => (
+                <div className="footerLink" key={header.textContent}>
                     <p>
-                        <a onClick={() => {header.scrollIntoView({behavior: 'smooth', block: 'center'})}}>
+                        <a onClick={() => header.scrollIntoView({ behavior: 'smooth', block: 'center' })}>
                             {header.textContent}
                         </a>
                     </p>
                 </div>
-            )
-        })
-        setTimeout(() => {
+            )));
+        };
 
-            setLinks(res);
-        }, 20);
-    }, [])
+        //try to build straight away
+        buildLinks();
+
+        //if we cannot build straight away then wait for headers to appear
+        const observer = new MutationObserver(buildLinks);
+        observer.observe(document.getElementById('content')!, {
+            childList: true,
+            subtree: true,
+        });
+
+        return () => observer.disconnect();
+    }, []);
 
     return (
         <React.Fragment>
